2da16618f53b1f33c731527a98c94e3e6620949e
nerjs <nerjs.stap@gmail.com>
2020-11-22 23:23:07 +0200

    Возврат к предыдущей инициализации схемы

--- main/schema/index.js
+++ main/schema/index.js
@@ -1,5 +1,10 @@
-const buildSchema = require('./buildSchema')
+const { mergeTypeDefs, mergeResolvers } = require('@graphql-tools/merge')
+const { loadFilesSync } = require('@graphql-tools/load-files')
+const path = require('path')
+const { makeExecutableSchema } = require('graphql-tools')
+const schemaDirectives = require('./directives')
 
-module.exports = buildSchema()
+const typeDefs = mergeTypeDefs(loadFilesSync(path.join(__dirname, 'types')))
+const resolvers = mergeResolvers(loadFilesSync(path.join(__dirname, 'resolvers')))
 
-// console.dir(module.exports, { depth: 10 })
+module.exports = makeExecutableSchema({ typeDefs, resolvers, schemaDirectives })
