4b05436fdc32e3ddb8047b541c383a9a52914ea4
nerjs <nerjs.stap@gmail.com>
2020-12-01 22:21:41 +0200

    Удаление старого апи

--- main/models/docker/constants.js
+++
@@ -1,7 +0,0 @@
-exports.EVENTS_MAX_FOR_TIME = 1000 * 60 * 60 * 24 * 30 * 100
-exports.EVENTS_MAX_RETRY = 30
-
-exports.CH = {
-  EVENTS: 'docker:events',
-  IMAGES: 1,
-}
--- main/models/index.js
+++
@@ -1,1 +0,0 @@
-exports.dockerModel = require('./docker')
